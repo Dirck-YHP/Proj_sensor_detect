7b3ed94def013c93ffa4ebf4cd2a056ff22bd4d8
DirckYHP <1941803238@qq.com>
2024-07-13 19:02:27 +0800

    添加电池电量计算

--- functionality/electricitycalc.cpp
+++ functionality/electricitycalc.cpp
@@ -4,3 +4,44 @@ ElectricityCalc::ElectricityCalc(QObject *parent) : QObject(parent)
 {
 
 }
+
+/***************************************************************
+  *  @brief     开始进行数据采集
+  *  @param     无
+  *  @note      构造数据采集类的对象，接收数据采集类传过来的数据并进行处理
+  *  @Sample usage:
+ **************************************************************/
+void ElectricityCalc::start_acquire()
+{
+    data_acquire_ai = new DataAcquireAI;
+    data_acquire_ai->get_channel(CHANNEL);
+    QThreadPool::globalInstance()->start(data_acquire_ai);
+
+    connect(data_acquire_ai, &DataAcquireAI::send_data,
+            this, &ElectricityCalc::rev_data_from_ni9205);
+}
+
+/***************************************************************
+  *  @brief     停止进行数据采集
+  *  @param     无
+  *  @note      停止9205的数据采集
+  *  @Sample usage:
+ **************************************************************/
+void ElectricityCalc::stop_acquire()
+{
+    data_acquire_ai->stop_acquire();
+}
+
+/***************************************************************
+  *  @brief     接收ni9205的数据并处理
+  *  @param     无
+  *  @note      转化成电池电量，然后送给上层
+  *  @Sample usage:
+ **************************************************************/
+void ElectricityCalc::rev_data_from_ni9205(QVector<double> data)
+{
+    // 目前还没做转化，直接发送原始数据
+    emit send_battery_to_ui(data);
+}
+
+
--- functionality/electricitycalc.h
+++ functionality/electricitycalc.h
@@ -2,6 +2,9 @@
 #define ELECTRICITYCALC_H
 
 #include <QObject>
+#include <QThreadPool>
+
+#include "data_acquire/dataacquireai.h"
 
 class ElectricityCalc : public QObject
 {
@@ -9,8 +12,22 @@ class ElectricityCalc : public QObject
 public:
     explicit ElectricityCalc(QObject *parent = nullptr);
 
-signals:
+private:
+    const QString CHANNEL = "31";           // 通道
 
+    DataAcquireAI *data_acquire_ai;     // NI 9205的数据
+
+public:
+    void start_acquire();
+    void stop_acquire();
+
+private slots:
+    // 这个函数负责接收来自采集卡的原始数据并做处理，然后发出去
+    void rev_data_from_ni9205(QVector<double> data);
+
+signals:
+    // 这里需要做转化，原始电压信号——>电池电量，发送的是电池电量
+   void send_battery_to_ui(QVector<double> data);
 };
 
 #endif // ELECTRICITYCALC_H
